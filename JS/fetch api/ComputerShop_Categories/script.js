205c93f682879e9da0784fbe76cf62b58fa46522
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-07-09 15:28:31 +0700

    test

--- JS/fetch api/ComputerShop_Categories/script.js
+++ JS/fetch api/ComputerShop_Categories/script.js
@@ -1,16 +1,16 @@
 const API_URL = "https://computer-shop-hit.herokuapp.com/api/categories";
-let datas;
 
 const renderData = (datas) => {
   const view = document.getElementById("view");
   datas.forEach((data) => {
     const htmlElement = `
-  <div>
-  <div>Category id: ${data.id}</div>
-  <div>Category name: ${data.name}</div>
-  <div> <p>Create at:</p> ${data.createAt}</div>
-  <div> <p>Update at:</p> ${data.updateAt}</div>
-  </div>`;
+      <div>
+      <div>Category id: ${data.id}</div>
+      <div>Category name: ${data.name}</div>
+      <div> <p>Create at:</p> ${data.createAt}</div>
+      <div> <p>Update at:</p> ${data.updateAt}</div>
+      </div>
+    `;
     view.innerHTML += htmlElement;
   });
 };
@@ -31,15 +31,3 @@ function getJSON() {
     });
 }
 getJSON();
-console.log(datas);
-
-// for (let i = 0; i < datas.length; i++) {
-//   const htmlElement = `
-//   <div>
-//   <div>${datas[i].id}</div>
-//   <div>${datas[i].name}</div>
-//   <div>${datas[i].createAt}</div>
-//   <div>${datas[i].updateAt}</div>
-//   </div>`;
-//   view.appendChild(htmlElement);
-// }
